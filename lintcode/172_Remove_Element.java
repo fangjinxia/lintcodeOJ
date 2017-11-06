e007c81fca3f00c26b16674ec3b351af31bee5e4
fangjinxia <fangjinxia@iie.ac.cn>
2017-11-06 19:51:19 +0800

    remove element

---
+++ lintcode/172_Remove_Element.java
@@ -0,0 +1,18 @@
+package lintcode;
+
+public class Remove_Element {
+	/*
+     * @param A: A list of integers
+     * @param elem: An integer
+     * @return: The new length after remove
+     */
+    public int removeElement(int[] A, int elem) {
+        int res = 0;
+        for(int i = 0; i < A.length; ++i){
+            if(A[i] != elem){
+                A[res++] = A[i];
+            } 
+        }
+        return res;
+    }
+}
